784223f76dba1e613a7a16ad190149cb5ca4b5f0
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2022-11-15 20:34:42 +0100

    Update project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,46 @@
-function Projects(){
+function Projects() {
+    const projects = [
+        {
+            name: "ToDo tutorial",
+            description: "Premier projet avec react, tutoriel de MDN",
+            url: "./logo512.png"
+        },
+        {
+            name: "Project 2",
+            description: "Premier projet avec react, tutoriel de MDN",
+            url: "./logo512.png"
+        },
+        {
+            name: "Project 3",
+            description: "Premier projet avec react, tutoriel de MDN",
+            url: "./logo512.png"
+        }
+    ];
+
+    const projectsList = projects.map((project, index) => {
+        if (index % 2 === 0) {
+            return (
+                <div className="single-project-container" key={index}>
+                    <img className="project-img-even" src={`${project.url}`} alt={project.name} />
+                    <h3 className="project-title-even">{project.name}</h3>
+                    <p className="project-text-even">{project.description}</p>
+                </div>);
+        }
+        else {
+            return (
+                <div className="single-project-container" key={index}>
+                    <h3 className="project-title-odd">{project.name}</h3>
+                    <p className="project-text-odd">{project.description}</p>
+                    <img className="project-img-odd" src={project.url} alt={project.name} />
+                </div>);
+        }
+    })
+
     return (
         <section className="projects-container" id="projects">
             <h2 className="section-title">Projects</h2>
             <div className="projects-list">
-                <div>
-                    <h3>Project 1</h3>
-                </div>
-                <div>
-                    <h3>Project 2</h3>
-                </div>
+                {projectsList}
             </div>
         </section>
     );
