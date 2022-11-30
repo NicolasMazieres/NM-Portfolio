321cd77d92f6cf98973e7f77aa8422fdeabc770c
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2022-11-30 19:27:06 +0100

    Update project logos display

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,17 +3,35 @@ function Projects() {
         {
             name: "Recipes website",
             description: "This projet is an online adaptation of a family recipe book. The purpose was to create a nice website to bring the cooking recipes everywhere we go. It was also a way to learn many concepts of web development with React.",
-            url: "/images/food-project.jpg"
+            url: "/images/food-project.jpg",
+            logos: [
+                "/images/html-logo.png",
+                "/images/css-logo.png",
+                "/images/js-logo.png",
+                "/images/react-logo.png"
+            ]
         },
         {
             name: "Todo tutorial",
             description: "Premier projet avec react, tutoriel de MDN",
-            url: "/images/todo-project.jpg"
+            url: "/images/todo-project.jpg",
+            logos: [
+                "/images/html-logo.png",
+                "/images/css-logo.png",
+                "/images/js-logo.png",
+                "/images/react-logo.png"
+            ]
         },
         {
             name: "Weather App",
             description: "Premier projet avec react, tutoriel de MDN",
-            url: "/images/weather-project.jpg"
+            url: "/images/weather-project.jpg",
+            logos: [
+                "/images/html-logo.png",
+                "/images/css-logo.png",
+                "/images/js-logo.png",
+                "/images/react-logo.png"
+            ]
         }
     ];
 
@@ -24,6 +42,14 @@ function Projects() {
                     <img className="project-img-even" src={`${project.url}`} alt={project.name} />
                     <h3 className="project-title-even">{project.name}</h3>
                     <p className="project-text-even">{project.description}</p>
+
+                    <div className="logos-container">
+                        {project.logos.map((logo, index) => {
+                            return (
+                                <img className="project-logos" src={logo} alt={`Logo n°${index}`} />
+                        )
+                        })}
+                    </div>
                 </div>);
         }
         else {
@@ -32,6 +58,14 @@ function Projects() {
                     <h3 className="project-title-odd">{project.name}</h3>
                     <p className="project-text-odd">{project.description}</p>
                     <img className="project-img-odd" src={project.url} alt={project.name} />
+
+                    <div className="logos-container">
+                        {project.logos.map((logo, index) => {
+                            return (
+                                <img className="project-logos" src={logo} alt={`Logo n°${index}`} />
+                        )
+                        })}
+                    </div>
                 </div>);
         }
     })
