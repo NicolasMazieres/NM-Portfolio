67d6406c86b598dd39a257966777277137fc59fd
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2022-12-08 20:59:32 +0100

    Switch Language button added

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,17 +2,23 @@ import { BsArrowDownCircle } from 'react-icons/bs';
 
 type Props = { handleArrowClick: React.MouseEventHandler<SVGElement> }
 
-function Landing({handleArrowClick}: Props) {
+function Landing({ handleArrowClick }: Props) {
     const handleClick = handleArrowClick;
 
     return (
         <section className="landing-container">
+            <div className="switch-button">
+                <input id="language-toggle" className="check-toggle check-toggle-round-flat" type="checkbox" />
+                <label htmlFor="language-toggle"></label>
+                <span className="on">FR</span>
+                <span className="off">EN</span>
+            </div>
             <p>
                 Hello, I'm Nicolas,
                 <br />
                 Welcome to my website !
             </p>
-            <BsArrowDownCircle className='down-arrow' onClick={handleClick}/>
+            <BsArrowDownCircle className='down-arrow' onClick={handleClick} />
         </section>
     );
 }
